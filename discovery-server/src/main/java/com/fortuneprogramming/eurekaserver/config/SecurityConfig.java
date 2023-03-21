eff9169d043999e2aa6221f00230b484f150579e
fortune <fortuneojih@yahoo.com>
2023-03-21 22:07:55 +0100

    created env for custom username and password

--- discovery-server/src/main/java/com/fortuneprogramming/eurekaserver/config/SecurityConfig.java
+++ discovery-server/src/main/java/com/fortuneprogramming/eurekaserver/config/SecurityConfig.java
@@ -1,19 +1,26 @@
 package com.fortuneprogramming.eurekaserver.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("${eureka.username}")
+    private String userName;
+    @Value("${eureka.password}")
+    private String password;
     //Basic authentication configuration
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("username").password("password")
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser(userName).password(password)
                 .authorities("USER");
     }
 
